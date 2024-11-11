5b9f7fa18651262ea98ab749959297a6f77fabc3
박종인 <paper@addd.co.kr>
2024-11-11 16:19:40 +0900

    commit test

--- front/src/components/layouts/sidebar/sidebar.tsx
+++ front/src/components/layouts/sidebar/sidebar.tsx
@@ -5,7 +5,18 @@ const Sidebar = (data: { data: boolean }) => {
 
   const width = data.data ? "100px" : "0px";
 
-  return <div id="sidebar" style={{ width }}></div>;
+  return (
+    <div id="sidebar" style={{ width }}>
+      <div>home</div>
+      <div>
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
